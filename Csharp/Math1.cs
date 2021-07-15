37363b3216e867f3f6b342e6c8984d14b9cad119
Choi-jinuk <wlsdnr7536@naver.com>
2021-07-15 20:13:43 +0900

    백준 2292, 벌집 문제

---
+++ Csharp/Math1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Math1
+{
+    static void Main(string[] args)
+    {
+        String[] input = Console.ReadLine().Split(' ');
+        int a = int.Parse(input[0]);
+        int b = int.Parse(input[1]);
+        int c = int.Parse(input[2]);
+
+        int profit = c - b;
+
+        if (profit <= 0)
+        {
+            Console.Write("-1");
+        }
+        else
+        {
+            Console.Write(a / profit + 1);
+        }
+    }
+}
---
+++ Csharp_WorkSpace/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Csharp_WorkSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int cnt = 1;//몇개의 방을 지나가야 되는지에 대한 변수
+            int power = 0;//값 비교를 위해 곱해줄 수
+            while((n - (6*power + 1)) > 0 )
+            {
+                power = power + cnt++;
+            }
+            Console.Write(cnt);
+        }
+    }
+}
