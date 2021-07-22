01d90cb859240b28b9ab4fb1b8680b863b4d74a1
Choi-jinuk <wlsdnr7536@naver.com>
2021-07-22 23:04:11 +0900

    백준 2869, 달팽이는 올라가고 싶다

---
+++ Csharp/Math4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Csharp_WorkSpace
+{
+    class Math4
+    {
+        static StreamReader input = new StreamReader(Console.OpenStandardInput());
+        static void Main(string[] args)
+        {
+            String[] line = input.ReadLine().Split(' ');
+            int a = int.Parse(line[0]);
+            int b = int.Parse(line[1]);
+            int v = int.Parse(line[2]);
+
+            int day = ((v-a)/(a-b)) + 1;
+            if((v-a)%(a-b) > 0) day++;
+
+            Console.Write(day);
+        }
+    }
+}
